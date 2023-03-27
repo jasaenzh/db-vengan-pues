759599ba9b5f37c20a6679fbb1442e32beb60143
Jhony A Saenz <jash0310@gmail.com>
2023-03-27 17:28:28 -0500

    montando controller apartamento v4

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const mainRouter = require("./routes/index.routes")
+const mainRouter = require("./routes/index.routes");
+const morgan = require("morgan");
 
 const app = express();
 
 app.set('port', process.env.PORT || 3001)
+
+// Middeleware para hacer trakin de lo ocuerre en servidor
+app.use(morgan("dev"));
 
 // Middeleware para transformar los datos que vengan como json en un objeto de JavaScript
 app.use(express.json());
